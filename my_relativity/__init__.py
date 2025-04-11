bacda6c7a4983ea9352e0b2fcb6836789c546d3c
lldddv <lldddv@gmail.com>
2025-04-11 12:21:34 -0500

    [FIX]

---
+++ my_relativity/__init__.py
@@ -0,0 +1,11 @@
+# my_relativity/__init__.py
+
+# Importar funciones/clases específicas de tus submódulos
+from . import plots
+
+# import *
+__all__ = ['plots']
+
+# Package information
+__version__ = '0.1.0'
+__author__ = 'Luis Daniel Díaz'
